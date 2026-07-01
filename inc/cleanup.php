c7fda0664ebb2319f3c65beb2bc86398e2d14688
khanhaxcv-commits <khanhaxcv@gmail.com>
2026-07-01 20:50:15 +0700

    flatsome-child: fix chữ đè lên nhau

--- inc/cleanup.php
+++ inc/cleanup.php
@@ -5,7 +5,7 @@ if (!defined('WPCF7_AUTOP_FILTER')) {
 }
 
 add_action('init', function () {
-    remove_filter('the_content', 'wpautop');
-    remove_filter('the_excerpt', 'wpautop');
-    remove_filter('comment_text', 'wpautop');
+    // remove_filter('the_content', 'wpautop');
+    // remove_filter('the_excerpt', 'wpautop');
+    // remove_filter('comment_text', 'wpautop');
 });
