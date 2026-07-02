414b03fad5ab9bb02a5a40df897b83e6d3d2285f
khanhaxcv-commits <khanhaxcv@gmail.com>
2026-07-02 23:13:25 +0700

    flatsome-child: thêm logic không nén các file thư muc không cần thiết trong pluggin All-in-One WP Migration

--- functions.php
+++ functions.php
@@ -9,8 +9,13 @@ $plana_theme_includes = array(
     'inc/blog-category.php',
     'inc/blog-single.php',
     'inc/preloader.php',
+    'inc/migration.php',
 );
 
 foreach ($plana_theme_includes as $plana_theme_include) {
-    require_once get_stylesheet_directory() . '/' . $plana_theme_include;
+    $plana_theme_file = get_stylesheet_directory() . '/' . $plana_theme_include;
+
+    if (file_exists($plana_theme_file)) {
+        require_once $plana_theme_file;
+    }
 }
